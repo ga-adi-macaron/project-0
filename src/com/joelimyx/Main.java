cfe1bdd68f63d61e45023a030b7fe649af76c6f9
Joe Lim <crazyfrogjoelim@gmail.com>
2016-10-06 15:32:24 -0400

    Fixed rps logic

--- src/com/joelimyx/Main.java
+++ src/com/joelimyx/Main.java
@@ -6,22 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void rockPaperScissor(String computerChoice, String user_Choice, String winCondition){
         //Rock
-        if (computerChoice.equals("rock")){
+        System.out.println("The computer pick "+computerChoice+".");
+        System.out.println("You pick "+ user_Choice+".");
+
+        if (computerChoice.equals("Rock")){
+            //Rock
             if(user_Choice.equalsIgnoreCase(computerChoice)){
                 System.out.println("It is a DRAW!");
                 winCondition = "Draw";
-            }else if (user_Choice.equalsIgnoreCase("scissor")){
+            }else if (user_Choice.equalsIgnoreCase("paper")){
                 System.out.println("You WiN!");
                 winCondition = "Won";
             }else{
                 System.out.println("You LOST...");
                 winCondition = "Lost";
             }
-        }else if(computerChoice.equals("paper")) {
+
+        }else if(computerChoice.equalsIgnoreCase("Paper")) {
+            //Computer paper
             if (user_Choice.equalsIgnoreCase(computerChoice)) {
                 System.out.println("It is a DRAW!");
                 winCondition = "Draw";
-            } else if (user_Choice.equalsIgnoreCase("rock")) {
+            } else if (user_Choice.equalsIgnoreCase("scissor")) {
                 System.out.println("You WiN!");
                 winCondition = "Won";
             } else {
@@ -29,10 +35,12 @@ public class Main {
                 winCondition = "Lost";
             }
         }else{
+
+            //Computer == scissor
             if(user_Choice.equalsIgnoreCase(computerChoice)){
                 System.out.println("It is a DRAW!");
                 winCondition = "Draw";
-            }else if (user_Choice.equalsIgnoreCase("paper")){
+            }else if (user_Choice.equalsIgnoreCase("rock")){
                 System.out.println("You WiN!");
                 winCondition = "Won";
             }else{
@@ -52,7 +60,7 @@ public class Main {
                 inHistory = true;
 
 
-        while (true){
+        while (mainMenu){
             //Game mode
             System.out.println("Welcome to a game of ROCK PAPER SCISSOR.");
             System.out.println("Please enter a game mode: (Play or History)");
@@ -60,7 +68,7 @@ public class Main {
             if(mode.equalsIgnoreCase("play")){
 
                 //Rock Paper Scissor
-                while(true) {
+                while(inGame) {
                     System.out.println("Enter your choice: (Rock , Paper or Scissor) or \"Quit\" to exit");
                     userChoice = input.nextLine();
 
@@ -71,23 +79,25 @@ public class Main {
                         //Computer Random
                         int rand = (int)(Math.random()*3);
                         switch (rand){
-                            case 1:
+                            case 0:
                                 computerChoice ="Rock";
                                 rockPaperScissor(computerChoice,userChoice,winOrLose);
                                 break;
-                            case 2:
+                            case 1:
                                 computerChoice = "Paper";
                                 rockPaperScissor(computerChoice,userChoice,winOrLose);
                                 break;
-                            case 3:
+                            case 2:
                                 computerChoice = "Scissor";
                                 rockPaperScissor(computerChoice,userChoice,winOrLose);
                                 break;
                         }
                         System.out.println("Do you want to play again?: (Y/N)");
                         if (input.nextLine().equalsIgnoreCase("n")){
-
+                            inGame = false;
                         }
+                    }else if(userChoice.equalsIgnoreCase("quit")){
+                        inGame = false;
                     }
                 }
             }else if (mode.equalsIgnoreCase("history")){
