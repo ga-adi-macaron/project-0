0da2d7014803f444cb0c1e884e45f5166873f489
Joe Lim <crazyfrogjoelim@gmail.com>
2016-10-07 12:07:17 -0400

    Memory storage

--- src/com/joelimyx/Main.java
+++ src/com/joelimyx/Main.java
@@ -1,6 +1,12 @@
 package com.joelimyx;
 
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -49,83 +55,144 @@ public class Main {
             }
         }
     }
+    public static void writeToFile(BufferedWriter fileToWrite, String phrase) throws IOException {
+        fileToWrite.write(phrase+"\r");
+        fileToWrite.newLine();
+    }
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        //******************************************************************************************
+        //Beginning of MAIN
+        //Variables
         Scanner input = new Scanner(System.in);
         ArrayList<String> historyList = new ArrayList<>();
+        ArrayList<String> readList = new ArrayList<>();
         int numOfGamePlayed = 0;
         String mode, userChoice, computerChoice="";
         boolean mainMenu = true,
                 inGame;
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-dd: hh:mm:ssa");
 
+        //******************************************************************************************
+        //FileReader
+        BufferedReader bufferedReader;
+        String fileName = "history.txt";
+        BufferedWriter writer;
+        String temp;
 
-        while (mainMenu){
-            //Game mode
-            System.out.println("Welcome to a game of ROCK PAPER SCISSOR.");
-            System.out.println("Choose a selection: (Play, History or Quit)");
-            mode = input.nextLine();
-            if(mode.equalsIgnoreCase("play")){
-
-                inGame = true;
-
-                //Rock Paper Scissor play mode
-                while(inGame) {
-                    System.out.println("Enter your choice: (Rock , Paper or Scissor) or \"Quit\" to exit");
-                    userChoice = input.nextLine();
-
-                    if(userChoice.equalsIgnoreCase("rock")
-                            || userChoice.equalsIgnoreCase("paper")
-                            || userChoice.equalsIgnoreCase("scissor")){
-
-                        String winOrLose="never";
-
-                        //Computer Random generator
-                        int rand = (int)(Math.random()*3);
-                        switch (rand){
-                            case 0:
-                                computerChoice ="Rock";
-                                winOrLose = rockPaperScissor(computerChoice,userChoice);
-                                break;
-                            case 1:
-                                computerChoice = "Paper";
-                                winOrLose = rockPaperScissor(computerChoice,userChoice);
-                                break;
-                            case 2:
-                                computerChoice = "Scissor";
-                                winOrLose = rockPaperScissor(computerChoice,userChoice);
-                                break;
-                        }
-                        //Storing History
-                        numOfGamePlayed++;
-                        historyList.add(numOfGamePlayed+". Computer played: "+
-                                computerChoice+". You played: "+userChoice+
-                                ". The result was "+winOrLose);
-
-                        while (inGame){
-                            System.out.println("Do you want to play again?: (Y/N)");
-                            if (input.nextLine().equalsIgnoreCase("n")){
-                                inGame = false;
-                            }
-                        }
-                    }else if(userChoice.equalsIgnoreCase("quit")){
-                        //Quit input
-                        inGame = false;
-                    }
-                }
-            // Print out History
-            }else if (mode.equalsIgnoreCase("history")){
-                for (String history :
-                        historyList) {
-                    System.out.println(history);
-                }
-
-            // Quit Game
-            }else if (mode.equalsIgnoreCase("quit")){
-                System.out.println("Thanks for playing. Have a nice day.");
-                mainMenu = false;
-
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            while ((temp = bufferedReader.readLine()) != null) {
+                readList.add(temp);
             }
-        }
+            writer = new BufferedWriter(new FileWriter(fileName));
+            for (String phrase :
+                    readList) {
+                writeToFile(writer, phrase);
+            }
+
+            //******************************************************************************************
+            //MainMenu
+            while (mainMenu) {
+
+                //******************************************************************************************
+                //Game mode
+                System.out.println("+====================================+\n" +
+                        "Welcome to a game of ROCK PAPER SCISSOR.");
+                System.out.println("Choose a selection: (Play, History or Quit)");
+                mode = input.nextLine();
+
+                //******************************************************************************************
+                //Rock Paper Scissor play mode
+                if (mode.equalsIgnoreCase("play")) {
+                    writeToFile(writer,currentDateTime.format(formatter));
+                    writer.flush();
+                    writeToFile(writer,"Session start:");
+
+
+                    inGame = true;
+
+                    while (inGame) {
+                        System.out.println("+========================================+\n" +
+                                "Enter your choice: (Rock , Paper or Scissor) \nor \"Quit\" to exit");
+                        userChoice = input.nextLine();
+
+                        if (userChoice.equalsIgnoreCase("rock")
+                                || userChoice.equalsIgnoreCase("paper")
+                                || userChoice.equalsIgnoreCase("scissor")) {
+
+                            String winOrLose = "never";
+
+                            //******************************************************************************************
+                            //Computer Random generator
+                            int rand = (int) (Math.random() * 3);
+                            switch (rand) {
+                                case 0:
+                                    computerChoice = "Rock";
+                                    winOrLose = rockPaperScissor(computerChoice, userChoice);
+                                    break;
+                                case 1:
+                                    computerChoice = "Paper";
+                                    winOrLose = rockPaperScissor(computerChoice, userChoice);
+                                    break;
+                                case 2:
+                                    computerChoice = "Scissor";
+                                    winOrLose = rockPaperScissor(computerChoice, userChoice);
+                                    break;
+                            }
+
+                            //******************************************************************************************
+                            //Storing History
+                            numOfGamePlayed++;
+                            writeToFile(writer,numOfGamePlayed + ". Computer played: " +
+                                    computerChoice + ". You played: " + userChoice +
+                                    ". The result was " + winOrLose);
+
+                            String decision = "i";
+                            while (inGame && !decision.equalsIgnoreCase("y")) {
+                                System.out.println("Do you want to play again?: (Y/N)");
+                                decision = input.nextLine();
+                                if (decision.equalsIgnoreCase("n")) {
+                                    inGame = false;
+                                    writeToFile(writer,currentDateTime.format(formatter));
+                                    writeToFile(writer,"Session ended.\r");
+                                }
+                            }
+                        } else if (userChoice.equalsIgnoreCase("quit")) {
+                            //Quit input
+                            inGame = false;
+                            writeToFile(writer,currentDateTime.format(formatter));
+                            writeToFile(writer,"Session ended.\r");
+                        }
+                    }
+                    //******************************************************************************************
+                    // Print out History
+                    writer.close();
+                } else if (mode.equalsIgnoreCase("history")) {
+                    while ((temp = bufferedReader.readLine()) != null) {
+                        readList.add(temp);
+                    }
+                    for (String line :
+                            readList) {
+                        System.out.println(line);
+                    }
+
+                    //******************************************************************************************
+                    // Quit Game
+                } else if (mode.equalsIgnoreCase("quit")) {
+                    System.out.println("Thanks for playing. Have a nice day.");
+                    mainMenu = false;
+                }
+            }
+                //******************************************************************************************
+                //Close out writer
+
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.getMessage();
+            }
     }
 }
+
